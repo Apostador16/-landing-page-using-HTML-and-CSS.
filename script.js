c5e96ad444652f9d7eadda306a96a2212faa2040
AngelTorres <hernandeztorresangelfernando@gmail.com>
2026-09-10 08:20:34 -0600

    ajuste de detalles e implementacion de images

--- script.js
+++ script.js
@@ -1,17 +1,14 @@
 
 const track = document.getElementById('track');
-
-
 const cards = Array.from(track.children);
 
+const duplicateCards = () => {
+    cards.forEach(card => {
+        const clone = card.cloneNode(true);
+        clone.setAttribute('aria-hidden', 'true');
+        track.appendChild(clone);
+    });
+};
 
-cards.forEach(card => {
-
-    const clone = card.cloneNode(true);
-    
-  
-    clone.setAttribute('aria-hidden', true);
-    
-   
-    track.appendChild(clone);
-});
+duplicateCards();
+duplicateCards();
